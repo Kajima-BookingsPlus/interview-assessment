592e367cd539ee51d27f72502063f4f9d803ada8
Shaban Karumba <shabankarumba@gmail.com>
2021-09-28 23:56:51 +0100

    Refactor email params

--- app/mailers/confirmation_mailer.rb
+++ app/mailers/confirmation_mailer.rb
@@ -1,8 +1,14 @@
 class ConfirmationMailer < ApplicationMailer
   def booking_confirmation_email
-    mail(to: params[:user].email,
+    mail(to: user.email,
          body: 'Your booking has been confirmed',
          content_type: "text/html",
          subject: 'Booking confirmed')
+  end
+
+  private
+
+  def user
+    params[:user]
   end
 end
