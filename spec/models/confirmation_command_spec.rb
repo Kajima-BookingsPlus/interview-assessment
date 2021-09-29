2505fd9d3ad94dbdf80cd3549b9ac3994a85d1d3
delaney burke <delaney@zero2one.ee>
2021-09-29 15:22:31 +0300

    Failing Test
    - failing test because booking confirmed_at not set
    - failing test because booking confirmation state not set
    
    Fix
    - check if user approved
    - change state to confirmed

--- spec/models/confirmation_command_spec.rb
+++ spec/models/confirmation_command_spec.rb
@@ -1,11 +1,31 @@
 class ConfirmationCommand
+  def initialize (params: params)
+    @params = params
+  end
+  def execute
 
+  end
 end
 
 RSpec.describe ConfirmationCommand, type: :model do
   context "Send No Confirmation message" do
 
-    expect(booking.state).
+    it 'should be confirmed given a valid booking' do
+      #given
+      user = double(:user, approved: true)
+      host = double(:host)
+      booking = double(:booking,user: user, host: host, start_time: DateTime.now + 5.days, state: "provisional")
+      allow(Booking).to receive(:find).and_return(booking)
+      params = {id: 1}
+      command = ConfirmationCommand.new(params: params)
+      #When
+      command.execute
+      #Then
+      expect(booking).to receive(:confirmed_at)
+      expect(booking).to receive(:state)
+      expect(booking).to receive(:confirmed)  
+    end
+    
   end
   
 
