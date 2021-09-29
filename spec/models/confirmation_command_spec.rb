c17d8efaa980d9d54808bc3c6b1ca1abf6862653
delaney burke <delaney@zero2one.ee>
2021-09-29 15:54:43 +0300

    Failing test
    Test failing because the confirmed at is not set on the booking
    
    Fix:
     - set the confirmed at to date time now

--- spec/models/confirmation_command_spec.rb
+++ spec/models/confirmation_command_spec.rb
@@ -8,6 +8,7 @@ class ConfirmationCommand
     if(@booking.user.approved?)
       if(@booking.state == "provisional")
         @booking.confirmed!
+        
       end
     end
 
@@ -16,14 +17,33 @@ end
 
 RSpec.describe ConfirmationCommand, type: :model do
   context "Send No Confirmation message" do
-
-    it 'should be confirmed given a valid booking' do
+    context "An approved user, a provisional booking in teh future " do
+      
+    end
+    
+    it 'should be confirmed given a valid booking - state confirmed' do
       #given
       user = double(:user, approved?: true)
       host = double(:host)
       booking = double(:booking,user: user, host: host, start_time: DateTime.now + 5.days, state: "provisional")
 
-      expect(booking).to receive(:confirmed!) {}
+      expect(booking).to receive(:confirmed!) 
+      params = {id: 1}
+      command = ConfirmationCommand.new(booking: booking)
+      #When
+      command.execute
+      #Then
+
+    end
+
+    it 'should be confirmed given a valid booking - confirmed at set' do
+      #given
+      user = double(:user, approved?: true)
+      host = double(:host)
+      booking = double(:booking,user: user, host: host, start_time: DateTime.now + 5.days, state: "provisional")
+
+      allow(booking).to receive(:confirmed!) 
+      expect(booking).to receive(:confirmed_at) 
       
       params = {id: 1}
       command = ConfirmationCommand.new(booking: booking)
