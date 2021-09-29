3c46c53a18039d13eabedcc246dffa29417d8d0d
delaney burke <delaney@zero2one.ee>
2021-09-29 15:35:29 +0300

    remove the extra epectations ! one per test

--- spec/models/confirmation_command_spec.rb
+++ spec/models/confirmation_command_spec.rb
@@ -1,8 +1,15 @@
 class ConfirmationCommand
-  def initialize (params: params)
-    @params = params
+  def initialize (booking: booking, send_confirmation_msg: false,confirmation_type: :email)
+    @booking = booking
+    @send_confirmation_msg = send_confirmation_msg
+    @confirmation_type = confirmation_type
   end
   def execute
+    if(@booking.user.approved?)
+      if(@booking.state == "provisional")
+
+      end
+    end
 
   end
 end
@@ -15,15 +22,13 @@ RSpec.describe ConfirmationCommand, type: :model do
       user = double(:user, approved: true)
       host = double(:host)
       booking = double(:booking,user: user, host: host, start_time: DateTime.now + 5.days, state: "provisional")
-      
+
       params = {id: 1}
       command = ConfirmationCommand.new(booking: booking)
       #When
       command.execute
       #Then
-      expect(booking).to receive(:confirmed_at)
       expect(booking).to receive(:state)
-      expect(booking).to receive(:confirmed)  
     end
     
   end
